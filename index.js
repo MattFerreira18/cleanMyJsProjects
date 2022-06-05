4e63d14e9912c6cbc524eaea4dc5671453c7ce7a
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-06-05 17:39:04 -0300

    feat: add possibility to customize entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const os = require('node:os');
+
+const { PROJECTS_DIR_ENTRYPOINT } = require('./.env.json');
+
+const HOME_DIR_ALIAS = '$HOME';
+
+const getAllAfterASubstring = (str, substr) =>
+  str.substring(str.indexOf(substr) + substr.length)
 
 const getDirentsInCurrDir = (dirSource, callback) =>
   fs.promises.readdir(dirSource, { withFileTypes: true })
@@ -16,14 +24,23 @@ const isValidDir = (dirent) =>
 const removeDir = (dirSource) =>
   fs.rmSync(dirSource, { recursive: true, force: true });
 
+const sanitizeEntrypoint = (entrypoint) =>
+  entrypoint.includes(HOME_DIR_ALIAS)
+    ? `${os.homedir}${getAllAfterASubstring(entrypoint, HOME_DIR_ALIAS)}`
+    : entrypoint;
+
 function searchAndDelNodeModules(source) {
   getDirentsInCurrDir(source, (dirents) => {
     dirents.forEach(dirent => {
-      return isNodeModules(dirent) 
-      ? removeDir(getDirSource(source, dirent)) 
-      : searchAndDelNodeModules(getDirSource(source, dirent));
+      return isNodeModules(dirent)
+        ? removeDir(getDirSource(source, dirent))
+        : searchAndDelNodeModules(getDirSource(source, dirent));
     });
   });
 }
 
-searchAndDelNodeModules(__dirname);
+const entrypoint = PROJECTS_DIR_ENTRYPOINT 
+  ? sanitizeEntrypoint(PROJECTS_DIR_ENTRYPOINT)
+  : __dirname;
+
+searchAndDelNodeModules(entrypoint);
