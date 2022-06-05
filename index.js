8e903356b3a8d717deeb6cf92714f3556f0838fa
MattFerreira18 <matheusferreira.dev@gmail.com>
2022-06-05 17:13:00 -0300

    refactor: improve code, create minor functions

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-function getAndRemoveNodeModules(source) {
-  fs.promises.readdir(source, { withFileTypes: true })
-    .then(dirContent => {
-      const dirents = dirContent.filter(dir => (
-        dir.isDirectory() && !dir.name.startsWith('.')
-        ));
-        
-        dirents.forEach(dirent => {
-          if (dirent.name === 'node_modules') {
-          fs.rmSync(path.resolve(source, dirent.name), { recursive: true, force: true });
-        } else {
-          getAndRemoveNodeModules(path.resolve(source, dirent.name));
-        }
-      })
+const getDirentsInCurrDir = (dirSource, callback) =>
+  fs.promises.readdir(dirSource, { withFileTypes: true })
+    .then(dirContent => dirContent.filter(isValidDir))
+    .then(callback);
+
+const getDirSource = (source, dirent) => path.resolve(source, dirent.name);
+
+const isNodeModules = (dirent) => dirent.name === 'node_modules';
+
+const isValidDir = (dirent) =>
+  dirent.isDirectory() && !dirent.name.startsWith('.');
+
+const removeDir = (dirSource) =>
+  fs.rmSync(dirSource, { recursive: true, force: true });
+
+function searchAndDelNodeModules(source) {
+  getDirentsInCurrDir(source, (dirents) => {
+    dirents.forEach(dirent => {
+      return isNodeModules(dirent) 
+      ? removeDir(getDirSource(source, dirent)) 
+      : searchAndDelNodeModules(getDirSource(source, dirent));
     });
+  });
 }
 
-getAndRemoveNodeModules(__dirname);
+searchAndDelNodeModules(__dirname);
